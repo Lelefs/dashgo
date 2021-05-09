0a670df316c45dc4834e80c2f7bfa8c77695aa2c
Leandro Finochio <lefinochio@hotmail.com>
2021-05-09 09:38:11 -0300

    C4, Data fetching e cache local, 1, 6/6

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,31 @@
 import { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext';
+import { makeServer } from '../services/mirage';
 
 import { theme } from '../styles/theme';
 
-function MyApp({ Component, pageProps}: AppProps) {
-  return (
-    <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
-    </ChakraProvider>
-  )
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
 }
 
-export default MyApp
+const queryClient = new QueryClient();
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </ChakraProvider>
+
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
+}
+
+export default MyApp;
